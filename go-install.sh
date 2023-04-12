8345c1f12f199c449ab3f9da9fa40951a7f57d44
Jayanth Reddy <jaya@platform9.com>
2023-04-12 17:38:32 +0530

    Renamed the go-install file

--- go-install.sh
+++
@@ -1,7 +0,0 @@
-wget https://go.dev/dl/go1.20.3.linux-amd64.tar.gz
-tar -C ${HOME}/.local -xzf go1.20.3.linux-amd64.tar.gz
-export PATH=$PATH:${HOME}/.local
-go version
-make
-make pf9-image
-make pf9-push
