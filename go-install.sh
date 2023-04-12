13baef22a1b90b33706977c46545b2846c555c1c
Jayanth Reddy <jaya@platform9.com>
2023-04-12 15:37:18 +0530

    Added go installation

--- go-install.sh
+++ go-install.sh
@@ -1,2 +1,8 @@
+wget https://go.dev/dl/go1.20.3.linux-amd64.tar.gz
+rm -rf /usr/local/go 
+sudo tar -C /usr/local -xzf go1.20.3.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+go version
+make
 make pf9-image
 make pf9-push
