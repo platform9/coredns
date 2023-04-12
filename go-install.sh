9bd985bd1999ec64db9420ad69b5ba45dc0457da
Jayanth Reddy <jaya@platform9.com>
2023-04-12 16:22:34 +0530

    Install go without sudo

--- go-install.sh
+++ go-install.sh
@@ -1,7 +1,7 @@
 wget https://go.dev/dl/go1.20.3.linux-amd64.tar.gz
-rm -rf /usr/local/go 
-sudo tar -C /usr/local -xzf go1.20.3.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
+mkdir ${HOME}/.local
+tar -C ${HOME}/.local -xzf go1.20.3.linux-amd64.tar.gz
+export PATH=$PATH:${HOME}/.local
 go version
 make
 make pf9-image
